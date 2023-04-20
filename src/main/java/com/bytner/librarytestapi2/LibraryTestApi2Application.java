0f3561897ba58b0beb986f2611fd57e9841e1a9b
hbytner <hubertbytner@gmail.com>
2023-04-20 23:21:39 +0200

    Start

---
+++ src/main/java/com/bytner/librarytestapi2/LibraryTestApi2Application.java
@@ -0,0 +1,13 @@
+package com.bytner.librarytestapi2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryTestApi2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryTestApi2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/bytner/librarytestapi2/LibraryTestApi2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bytner.librarytestapi2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryTestApi2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
